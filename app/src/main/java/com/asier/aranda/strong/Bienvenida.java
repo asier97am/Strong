bcfeb957ac469e404d979fce801bdd97b321da6e
Mario Muñoz Fuentes <mario.munoz.fuentes>
2022-01-24 12:08:34 +0100

    Colores

--- app/src/main/java/com/asier/aranda/strong/Bienvenida.java
+++ app/src/main/java/com/asier/aranda/strong/Bienvenida.java
@@ -4,13 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class Bienvenida extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_bienvenida);
 
-        //Mario
     }
 }
