c9af65dd9696bb6cb3b1231e5bb5cdaa37e7c6e6
Asier Aranda <asier.aranda.malagon@gmail.com>
2022-01-31 14:03:48 +0100

    falta retocar bottom y ajustarlo

--- app/src/main/java/com/asier/aranda/strong/fragment/Page2.java
+++ app/src/main/java/com/asier/aranda/strong/fragment/Page2.java
@@ -1,13 +1,16 @@
 package com.asier.aranda.strong.fragment;
 
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.fragment.app.Fragment;
 
 import com.asier.aranda.strong.R;
+import com.bumptech.glide.Glide;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -50,6 +53,13 @@ public class Page2 extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        ImageView mback =findViewById(R.id.backView);
+//        Glide.with(this)
+//                .load(R.drawable.fondo3)
+//                .placeholder(new ColorDrawable(this.getResources().getColor(R.color.negroApp)))
+//                .centerCrop()
+//                .into(mback);
+
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
@@ -59,7 +69,12 @@ public class Page2 extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        //ImageView mback= new ImageView(.findViewById(R.id.backView));
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_page2, container, false);
     }
+
+
 }
+
