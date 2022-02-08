c92c52cd48f3ed896c55094d4d89d02b91769c51
Mario Muñoz Fuentes <mario.munoz.fuentes>
2022-02-08 10:33:32 +0100

    fotos añadidas

--- app/src/main/java/com/asier/aranda/strong/Animation.java
+++ app/src/main/java/com/asier/aranda/strong/Animation.java
@@ -2,7 +2,12 @@ package com.asier.aranda.strong;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.opengl.Visibility;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Animation extends AppCompatActivity {
 
@@ -10,5 +15,15 @@ public class Animation extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_animation);
+
+        // IMPLEMENTAR ANIMACION
+        TextView tv1= (TextView) findViewById(R.id.tv1);
+        android.view.animation.Animation blink= AnimationUtils.loadAnimation(this, R.anim.blink);
+        tv1.startAnimation(blink);
+
+
+        ImageView iv1= (ImageView) findViewById(R.id.iv1);
+        android.view.animation.Animation blink1= AnimationUtils.loadAnimation(this, R.anim.blink);
+        iv1.startAnimation(blink1);
     }
 }
