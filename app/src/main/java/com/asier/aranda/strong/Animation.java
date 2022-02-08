158872a1e98f7a6604a5f001ac37be2fdf02df05
Mario Muñoz Fuentes <mario.munoz.fuentes>
2022-02-08 11:48:23 +0100

    Animacion Splash acabada completa

--- app/src/main/java/com/asier/aranda/strong/Animation.java
+++ app/src/main/java/com/asier/aranda/strong/Animation.java
@@ -19,47 +19,47 @@ public class Animation extends AppCompatActivity {
 
         // IMPLEMENTAR ANIMACION
         // animacion imagen1
-        ImageView iv1= (ImageView) findViewById(R.id.iv1);
+        ImageView iv1= findViewById(R.id.iv1);
         android.view.animation.Animation blink1= AnimationUtils.loadAnimation(this, R.anim.blink1);
         iv1.startAnimation(blink1);
 
         // animacion imagen2
-        ImageView iv2= (ImageView) findViewById(R.id.iv2);
+        ImageView iv2= findViewById(R.id.iv2);
         android.view.animation.Animation blink2= AnimationUtils.loadAnimation(this, R.anim.blink2);
         iv2.startAnimation(blink2);
 
         // animacion imagen3
-        ImageView iv3= (ImageView) findViewById(R.id.iv3);
+        ImageView iv3= findViewById(R.id.iv3);
         android.view.animation.Animation blink3= AnimationUtils.loadAnimation(this, R.anim.blink3);
         iv3.startAnimation(blink3);
 
         // animacion imagen4
-        ImageView iv4= (ImageView) findViewById(R.id.iv4);
+        ImageView iv4= findViewById(R.id.iv4);
         android.view.animation.Animation blink4= AnimationUtils.loadAnimation(this, R.anim.blink4);
         iv4.startAnimation(blink4);
 
         // animacion imagen5
-        ImageView iv5= (ImageView) findViewById(R.id.iv5);
+        ImageView iv5= findViewById(R.id.iv5);
         android.view.animation.Animation blink5= AnimationUtils.loadAnimation(this, R.anim.blink5);
         iv5.startAnimation(blink5);
 
         // animacion imagen6
-        ImageView iv6= (ImageView) findViewById(R.id.iv6);
+        ImageView iv6= findViewById(R.id.iv6);
         android.view.animation.Animation blink6= AnimationUtils.loadAnimation(this, R.anim.blink6);
         iv6.startAnimation(blink6);
 
         // animacion imagen7
-        ImageView iv7= (ImageView) findViewById(R.id.iv7);
+        ImageView iv7= findViewById(R.id.iv7);
         android.view.animation.Animation blink7= AnimationUtils.loadAnimation(this, R.anim.blink7);
         iv7.startAnimation(blink7);
 
         // animacion imagen8
-        ImageView iv8= (ImageView) findViewById(R.id.iv8);
+        ImageView iv8= findViewById(R.id.iv8);
         android.view.animation.Animation blink8= AnimationUtils.loadAnimation(this, R.anim.blink8);
         iv8.startAnimation(blink8);
 
         // animacion imagen9
-        ImageView iv9= (ImageView) findViewById(R.id.iv9);
+        ImageView iv9= findViewById(R.id.iv9);
         android.view.animation.Animation blink9= AnimationUtils.loadAnimation(this, R.anim.blink9);
         iv9.startAnimation(blink9);
 
