87c2efa3dd7103d8103158ce0ced904073f07e2b
Wilson-Camacho <wilsonc.act@gmail.com>
2022-02-14 13:57:06 +0100

    Creando BBDD Persona V3.

---
+++ app/src/main/java/com/asier/aranda/strong/Persona.java
@@ -0,0 +1,132 @@
+package com.asier.aranda.strong;
+
+public class Persona {
+    private String identificador;
+    private String email;
+    private String password;
+    private String username;
+    private String apellido;
+
+    private int edad;
+    private Float peso;
+    private Float altura;
+
+    private String genero;
+    private String actividad;
+
+
+    public Persona(String email, String password, String username, String apellido, int edad, Float peso, Float altura, String genero, String actividad, String identificador) {
+        this.email = email;
+        this.password = password;
+        this.username = username;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.peso = peso;
+        this.altura = altura;
+        this.genero = genero;
+        this.actividad = actividad;
+        this.identificador = identificador;
+    }
+
+    public Persona(){}
+
+    //INICIO GETTERS Y SETTERS
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public Float getPeso() {
+        return peso;
+    }
+
+    public void setPeso(Float peso) {
+        this.peso = peso;
+    }
+
+    public Float getAltura() {
+        return altura;
+    }
+
+    public void setAltura(Float altura) {
+        this.altura = altura;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getActividad() {
+        return actividad;
+    }
+
+    public void setActividad(String actividad) {
+        this.actividad = actividad;
+    }
+
+    public String getIdentificador() {
+        return identificador;
+    }
+
+    public void setIdentificador(String identificador) {
+        this.identificador = identificador;
+    }
+
+    //FIN GETTERS Y SETTERS
+
+
+    @Override
+    public String toString() {
+        return "Persona{" +
+                "identificador='" + identificador + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", username='" + username + '\'' +
+                ", apellido='" + apellido + '\'' +
+                ", edad=" + edad +
+                ", peso=" + peso +
+                ", altura=" + altura +
+                ", genero='" + genero + '\'' +
+                ", actividad='" + actividad + '\'' +
+                '}';
+    }
+}
