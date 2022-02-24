90e3da75fbdea5e6a66d42bf2d09ad54df0add31
Mario Muñoz Fuentes <mario.munoz.fuentes>
2022-02-24 09:02:20 +0100

    Ultimos retoques

--- app/src/main/java/com/asier/aranda/strong/fragment/Page3.java
+++ app/src/main/java/com/asier/aranda/strong/fragment/Page3.java
@@ -2,6 +2,7 @@ package com.asier.aranda.strong.fragment;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.view.LayoutInflater;
