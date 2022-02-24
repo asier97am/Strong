a26e10ee6fc4b69fa3e012652bd37036ffcbcdc9
Asier97am <asier.aranda.malagon@gmail.com>
2022-02-24 18:45:01 +0100

    modificando ALERT DIALOG FRAGMENT 3

--- app/src/main/java/com/asier/aranda/strong/EjerciciosHome.java
+++ app/src/main/java/com/asier/aranda/strong/EjerciciosHome.java
@@ -1,12 +1,13 @@
 package com.asier.aranda.strong;
 
 public class EjerciciosHome {
-    private String ejercicio,descripcion;
+    private String ejercicio,descripcion,descripcion2;
     private int fotoEjercicio;
 
-    public EjerciciosHome(String ejercicio, String descripcion, int fotoEjercicio) {
+    public EjerciciosHome(String ejercicio, String descripcion,String descripcion2, int fotoEjercicio) {
         this.ejercicio = ejercicio;
         this.descripcion = descripcion;
+        this.descripcion2 = descripcion2;
         this.fotoEjercicio = fotoEjercicio;
     }
 
@@ -24,6 +25,14 @@ public class EjerciciosHome {
 
     public void setDescripcion(String descripcion) {
         this.descripcion = descripcion;
+    }
+
+    public String getDescripcion2() {
+        return descripcion2;
+    }
+
+    public void setDescripcion2(String descripcion2) {
+        this.descripcion2 = descripcion2;
     }
 
     public int getFotoEjercicio() {
